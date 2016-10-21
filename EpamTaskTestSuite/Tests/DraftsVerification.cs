5f03eb126a1d69c3b28f8c48feba10b86ecd7235
Aleksei_Voronin <ralexeyr@gmail.com>
2016-10-21 16:54:26 +0300

    implement data providers (User and Mail)

--- EpamTaskTestSuite/Tests/DraftsVerification.cs
+++ EpamTaskTestSuite/Tests/DraftsVerification.cs
@@ -3,23 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using EpamTaskTestSuite.DataProviders;
+using EpamTaskTestSuite.Entities;
 using NUnit.Framework;
 
 namespace EpamTaskTestSuite
 {
     [TestFixture]
+    [TestFixtureSource(typeof(UserProvider), nameof(UserProvider.Users))]
     public class DraftsVerification
     {
+        private User _user;
+
+        public DraftsVerification(User user)
+        {
+            _user = user;
+        }
+
         [SetUp]
         public void SetUp()
         {
             EpamTaskCoreLibrary.ActionProvider.Start();
             EpamTaskCoreLibrary.ActionProvider.NavigateToBase();
             // Precon 1. Login
-            //EpamTaskCoreLibrary.Pages.MainPage.Login(login, password, "");
+            EpamTaskCoreLibrary.Pages.MainPage.Login(_user.Name, _user.Password, "");
             // Precon 2. Verify that you were successfully logged in
-            //Assert.AreEqual(login + '@' + domain, EpamTaskCoreLibrary.Pages.MainPage.GetCurrentUserEmail());
+            Assert.AreEqual($"{_user.Name}@{_user.Domain}", EpamTaskCoreLibrary.Pages.MainPage.GetCurrentUserEmail());
         }
 
         [TearDown]
@@ -31,11 +40,9 @@ namespace EpamTaskTestSuite
         }
 
         [Test]
-        [TestCase("aivanooooov", "123qwe123", "mail.ru")]
-        public void VerifySavedLetterMovedToDrafts(string login, string password, string extension)
+        [TestCaseSource(typeof(MailProvider), nameof(MailProvider.Mails))]
+        public void VerifySavedLetterMovedToDrafts(Mail email)
         {
-            // ToDo: Step 4 - вынести данные в DataProvider
-
             // Step 1. Go to drafts
             EpamTaskCoreLibrary.Pages.DraftsPage.Open();
             // Step 2. Store number of draft letters
@@ -43,7 +50,7 @@ namespace EpamTaskTestSuite
             // Step 3. Go to composing new letter
             EpamTaskCoreLibrary.Pages.ComposeLetter.Open();
             // Step 4. Fill in required fields
-            EpamTaskCoreLibrary.Pages.ComposeLetter.WriteLetter("ivandseqweov@mailtest.ru", "Subject of letter111", "text-text-text");
+            EpamTaskCoreLibrary.Pages.ComposeLetter.WriteLetter(email.Recipient, email.Subject, email.Text);
             // Step 5. Save to drafts
             EpamTaskCoreLibrary.Pages.ComposeLetter.SaveLetterToDrafts();
             // Step 6. Go to drafts
@@ -53,11 +60,9 @@ namespace EpamTaskTestSuite
         }
 
         [Test]
-        [TestCase("aivanooooov", "123qwe123", "mail.ru")]
-        public void VerifySavedLetterHasCorrectSubject(string login, string password, string extension)
+        [TestCaseSource(typeof(MailProvider), nameof(MailProvider.Mails))]
+        public void VerifySavedLetterHasCorrectSubject(Mail email)
         {
-            // ToDo: Step 4 и 7 - вынести данные в DataProvider
-
             // Step 1. Go to drafts
             EpamTaskCoreLibrary.Pages.DraftsPage.Open();
             // Step 2. Store number of draft letters
@@ -65,13 +70,13 @@ namespace EpamTaskTestSuite
             // Step 3. Go to composing new letter
             EpamTaskCoreLibrary.Pages.ComposeLetter.Open();
             // Step 4. Fill in required fields
-            EpamTaskCoreLibrary.Pages.ComposeLetter.WriteLetter("ivandseqweov@mailtest.ru", "Subject of letter111", "text-text-text");
+            EpamTaskCoreLibrary.Pages.ComposeLetter.WriteLetter(email.Recipient, email.Subject, email.Text);
             // Step 5. Save to drafts
             EpamTaskCoreLibrary.Pages.ComposeLetter.SaveLetterToDrafts();
             // Step 6. Go to drafts
             EpamTaskCoreLibrary.Pages.DraftsPage.Open();
             // Step 7. Verify subject
-            Assert.AreEqual("Subject of letter111", EpamTaskCoreLibrary.Pages.DraftsPage.GetSubjectOfFirstDraftLetter());
+            Assert.AreEqual(email.Subject, EpamTaskCoreLibrary.Pages.DraftsPage.GetSubjectOfFirstDraftLetter());
         }
 
     }
